2fcdfce3d6c0c8736b6aae562388c339da7d6804
pavangpv <pavandhanu20@gmail.com>
2022-06-01 09:47:01 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <Switch>
+<Route exact pathh="/">
+  <Login />
+</Route>
+       </Switch>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+import React from 'react'
+
+const Login = (props) => {
+  return (
+      <container>
+          <content>
+              Content
+          </content>
+      </container>
+  )
+}
+
+// const Container = styled.section`
+// overFlow: hidden;
+// display: flex;
+// flex-direction:
+// `
+// const Content = styled.div`;`
+export default Login;
